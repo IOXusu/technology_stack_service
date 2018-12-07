22e5c27454070d358f2a032310791a9b3b50916f
xu.su <xu.su@huadongmedia.com>
2018-12-07 18:26:32 +0800

    feat:初始化项目代码

---
+++ src/main/java/com/xusu/technologystack/TechnologystackApplication.java
@@ -0,0 +1,12 @@
+package com.xusu.technologystack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnologystackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechnologystackApplication.class, args);
+    }
+}
---
+++ src/test/java/com/xusu/technologystack/TechnologystackApplicationTests.java
@@ -0,0 +1,16 @@
+package com.xusu.technologystack;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TechnologystackApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
